ef8fc28d18c9d1f027cb814d86207c3227f288f3
asserhanafy <asserhanafy68@gmail.com>
2024-09-15 02:19:49 +0300

    RPS Workshop

--- ros1_ws/build/my_py_pkg/prefix_override/sitecustomize.py
+++ ros1_ws/build/my_py_pkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/asserhanafy/ros1_ws/install/my_py_pkg'
+    sys.prefix = sys.exec_prefix = '/home/asserhanafy/ROS-Workshop/ros1_ws/install/my_py_pkg'
